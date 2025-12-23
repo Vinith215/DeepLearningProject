12505500be0368ec0bfd4cef7ba920ea1688f03e
Vinith Kumar <guntupallivineeth72@gmail.com>
2025-12-23 16:07:54 -0600

    model trainer added

--- Xray/ML/Model/__init__.py
+++ Xray/ML/Model/__init__.py
@@ -1,5 +1,5 @@
 # Compatibility shim: expose the contents of the original `Model` package
 # so imports using `Xray.ml.model` (lowercase) continue to work on all platforms.
-from Xray.ml.Model.arch import Net
+from .arch import Net
 
 __all__ = ["Net"]
---
+++ Xray/components/arch.py
@@ -0,0 +1,3 @@
+from Xray.ML.Model.arch import Net
+
+__all__ = ["Net"]
